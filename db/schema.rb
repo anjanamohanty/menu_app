d6e4421c104fba732cf38e38cd7cd5dd34e2fc38
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-03-07 22:29:09 -0500

    Add Course model and associations with Dish

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308031718) do
+ActiveRecord::Schema.define(version: 20160308032315) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image_file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dishes", force: :cascade do |t|
     t.integer  "course_id"
